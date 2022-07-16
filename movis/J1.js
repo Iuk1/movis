8951b9bf85dafabf5e985c811d63f2e1b4f9f2c1
IRUSHA UDARA <78417212+Iuk1@users.noreply.github.com>
2022-07-16 15:10:22 +0530

    Add files via upload

---
+++ movis/J1.js
@@ -0,0 +1,74 @@
+btns = document.getElementsByClassName("btn-book");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+			alert("Movie Booked!")
+        });
+    }
+
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+/*==================================LogIN=============================================*/
+function validate() {
+  var username = document.getElementById("un").value;
+
+  var pass = document.getElementById("pw").value;
+
+  if (username == "admin" && pass == "1234") {
+    alert("Login is Succesfully");
+    return false;
+  } else {
+    alert("login is failed");
+  }
+}
+
+/*==================Evants===========================*/
+
+
